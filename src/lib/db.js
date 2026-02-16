b6e85ad4aeb3700508359dff6b2027f746ae0ef4
tsmith608 <trentonsmith608@gmail.com>
2026-02-16 00:13:29 -0500

    fix db ssl

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,6 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : undefined,
 });
 
 export const query = (text, params) => pool.query(text, params);
